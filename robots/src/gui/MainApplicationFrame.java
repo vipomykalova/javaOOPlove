ff15b367a4c685f8a887e3104e5661d6c321310f
Viktoria <43198594+vipomykalova@users.noreply.github.com>
2019-03-20 20:02:00 +0500

    возможность редактировать положение жука при сохр

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -12,7 +12,7 @@ import log.Logger;
 
 /**
  * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
+ * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
  *
  */
@@ -23,15 +23,15 @@ public class MainApplicationFrame extends JFrame
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
         //of the screen.
-        int inset = 50;        
+        int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
+                screenSize.width  - inset*2,
+                screenSize.height - inset*2);
 
         setContentPane(desktopPane);
-        
-        
+
+
         LogWindow logWindow = createLogWindow();
         addWindow(logWindow);
 
@@ -42,7 +42,7 @@ public class MainApplicationFrame extends JFrame
         setJMenuBar(generateMenuBar());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
+
     protected LogWindow createLogWindow()
     {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
@@ -53,21 +53,21 @@ public class MainApplicationFrame extends JFrame
         Logger.debug("Протокол работает");
         return logWindow;
     }
-    
+
     protected void addWindow(JInternalFrame frame)
     {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-    
+
 //    protected JMenuBar createMenuBar() {
 //        JMenuBar menuBar = new JMenuBar();
-// 
+//
 //        //Set up the lone menu.
 //        JMenu menu = new JMenu("Document");
 //        menu.setMnemonic(KeyEvent.VK_D);
 //        menuBar.add(menu);
-// 
+//
 //        //Set up the first menu item.
 //        JMenuItem menuItem = new JMenuItem("New");
 //        menuItem.setMnemonic(KeyEvent.VK_N);
@@ -76,7 +76,7 @@ public class MainApplicationFrame extends JFrame
 //        menuItem.setActionCommand("new");
 ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        //Set up the second menu item.
 //        menuItem = new JMenuItem("Quit");
 //        menuItem.setMnemonic(KeyEvent.VK_Q);
@@ -85,19 +85,19 @@ public class MainApplicationFrame extends JFrame
 //        menuItem.setActionCommand("quit");
 ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        return menuBar;
 //    }
-    
+
     private JMenuBar generateMenuBar()
     {
         JMenuBar menuBar = new JMenuBar();
-        
+
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
         lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
         lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
                 "Управление режимом отображения приложения");
-        
+
         {
             JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
             systemLookAndFeel.addActionListener((event) -> {
@@ -120,7 +120,7 @@ public class MainApplicationFrame extends JFrame
         testMenu.setMnemonic(KeyEvent.VK_T);
         testMenu.getAccessibleContext().setAccessibleDescription(
                 "Тестовые команды");
-        
+
         {
             JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
             addLogMessageItem.addActionListener((event) -> {
@@ -154,8 +154,9 @@ public class MainApplicationFrame extends JFrame
 
     private JInternalFrame createSaveWindow(){
         gameWindow.getVisualizer().stopTimer();
-        JInternalFrame saveWindow = new JInternalFrame("Введите название", false, false,false,false);
-        saveWindow.setLocation(10,10);
+        gameWindow.getVisualizer().isEditor = true;
+        JInternalFrame saveWindow = new JInternalFrame("Введите название", false, true,false,false);
+        saveWindow.setLocation(420,0);
         saveWindow.setSize(300, 100);
 
         JPanel panel = new JPanel(new BorderLayout());
@@ -177,7 +178,8 @@ public class MainApplicationFrame extends JFrame
                     }
                     writer.flush();
                     writer.close();
-                    desktopPane.remove(saveWindow);
+                    saveWindow.dispose();
+                    gameWindow.getVisualizer().isEditor = false;
                     gameWindow.getVisualizer().setTimer();
                 } catch (IOException e1) {
                     e1.printStackTrace();
@@ -193,8 +195,8 @@ public class MainApplicationFrame extends JFrame
     }
 
     private JInternalFrame createLoadWindow(){
-        JInternalFrame saveWindow = new JInternalFrame("Введите название", false, false,false,false);
-        saveWindow.setLocation(10,10);
+        JInternalFrame saveWindow = new JInternalFrame("Введите название", false, true,false,false);
+        saveWindow.setLocation(420,0);
         saveWindow.setSize(300, 100);
 
         JPanel panel = new JPanel(new BorderLayout());
@@ -219,14 +221,14 @@ public class MainApplicationFrame extends JFrame
                     int tX = Integer.parseInt(gameState.get(3));
                     int tY = Integer.parseInt(gameState.get(4));
                     gameWindow.getVisualizer().setPosition(rX, rY, dir, tX, tY);
-                    desktopPane.remove(saveWindow);
+                    saveWindow.dispose();
                 }catch (FileNotFoundException e2) {
                     JOptionPane.showMessageDialog(saveWindow, "Файла с таким именем нет!",
                             "Ошибка загрузки", JOptionPane.WARNING_MESSAGE);
                 }
                 catch (IOException e1) {
                     e1.printStackTrace();
-            }
+                }
             }
         });
 
@@ -245,7 +247,7 @@ public class MainApplicationFrame extends JFrame
             SwingUtilities.updateComponentTreeUI(this);
         }
         catch (ClassNotFoundException | InstantiationException
-            | IllegalAccessException | UnsupportedLookAndFeelException e)
+                | IllegalAccessException | UnsupportedLookAndFeelException e)
         {
             // just ignore
         }
