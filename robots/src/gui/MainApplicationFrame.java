6a5abd709e8cffe13349ffaf34181f2b03ab4f78
Pkononsky <43210059+Pkononsky@users.noreply.github.com>
2019-03-29 10:59:32 +0500

    диалоговые окна

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -12,25 +12,25 @@ import javax.swing.event.InternalFrameEvent;
 import javax.swing.event.InternalFrameListener;
 
 import log.Logger;
+import sun.nio.cs.ext.EUC_JP_Open;
 
 /**
  * Что требуется сделать:
  * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
  */
-public class MainApplicationFrame extends JFrame
-{
+public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
     public GameWindow gameWindow;
+
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
         //of the screen.
         int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-                screenSize.width  - inset*2,
-                screenSize.height - inset*2);
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
 
         setContentPane(desktopPane);
 
@@ -39,17 +39,16 @@ public class MainApplicationFrame extends JFrame
         addWindow(logWindow);
 
         gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
+        gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
         setJMenuBar(generateMenuBar());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    protected LogWindow createLogWindow()
-    {
+    protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
+        logWindow.setLocation(10, 10);
         logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
@@ -57,8 +56,7 @@ public class MainApplicationFrame extends JFrame
         return logWindow;
     }
 
-    protected void addWindow(JInternalFrame frame)
-    {
+    protected void addWindow(JInternalFrame frame) {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
@@ -92,8 +90,7 @@ public class MainApplicationFrame extends JFrame
 //        return menuBar;
 //    }
 
-    private JMenuBar generateMenuBar()
-    {
+    private JMenuBar generateMenuBar() {
         JMenuBar menuBar = new JMenuBar();
 
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
@@ -135,16 +132,16 @@ public class MainApplicationFrame extends JFrame
         JMenu saveMenu = new JMenu("Сохранить/Загрузить");
         saveMenu.setMnemonic(KeyEvent.VK_T);
         {
-            JMenuItem save = new JMenuItem("Сохранить", KeyEvent.VK_S);
+            JMenuItem save = new JMenuItem("Save", KeyEvent.VK_S);
             save.addActionListener((event) -> {
-                addWindow(createSaveWindow());
+                saveState(saveMenu);
             });
             saveMenu.add(save);
         }
         {
             JMenuItem load = new JMenuItem("Загрузить", KeyEvent.VK_S);
             load.addActionListener((event) -> {
-                addWindow(createLoadWindow());
+                loadState(saveMenu);
             });
             saveMenu.add(load);
         }
@@ -155,120 +152,63 @@ public class MainApplicationFrame extends JFrame
         return menuBar;
     }
 
-    private JInternalFrame createSaveWindow(){
+    public void saveState(JMenu saveMenu) {
         gameWindow.getVisualizer().stopTimer();
-        gameWindow.getVisualizer().isEditor = true;
-        ArrayList<String> stateBeforeSave = gameWindow.getVisualizer().getGameState();
-        JInternalFrame saveWindow = new JInternalFrame("Введите название", false, true,false,false);
-        saveWindow.setLocation(420,0);
-        saveWindow.setSize(300, 100);
-
-        JPanel panel = new JPanel(new BorderLayout());
-        JTextField textField = new JTextField();
-        JButton button = new JButton("Сохранить");
-
-        saveWindow.addInternalFrameListener(new InternalFrameAdapter() {
-            @Override
-            public void internalFrameClosing(InternalFrameEvent e) {
-                gameWindow.getVisualizer().isEditor = false;
-                gameWindow.getVisualizer().setPosition(stateBeforeSave);
-                gameWindow.getVisualizer().setTimer();
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setCurrentDirectory(new File("src//saves"));
+        int ret = jFileChooser.showSaveDialog(saveMenu);
+        if (ret == jFileChooser.APPROVE_OPTION) {
+            try {
+                FileWriter fw = new FileWriter(jFileChooser.getSelectedFile() + ".txt");
+                ArrayList<String> state = gameWindow.getVisualizer().getGameState();
+                for (int i = 0; i < state.size(); i++) {
+                    fw.write(state.get(i) + " ");
+                }
+                fw.flush();
+                fw.close();
+            } catch (Exception e) {
 
             }
-        });
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                saveState(saveWindow, textField, stateBeforeSave);
-            }
-
-        });
-
-        panel.add(button, BorderLayout.SOUTH);
-        panel.add(textField, BorderLayout.NORTH);
-        saveWindow.add(panel);
-        return saveWindow;
-
+        }
+        gameWindow.getVisualizer().setTimer();
     }
 
-    private JInternalFrame createLoadWindow(){
-        JInternalFrame saveWindow = new JInternalFrame("Введите название", false, true,false,false);
-        saveWindow.setLocation(420,0);
-        saveWindow.setSize(300, 100);
+    public void loadState(JMenu saveMenu) {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setCurrentDirectory(new File("src//saves"));
+        int ret = jFileChooser.showOpenDialog(saveMenu);
+        if (ret == jFileChooser.APPROVE_OPTION) {
+            try {
+                File file = jFileChooser.getSelectedFile();
+                FileReader fw = new FileReader(file);
+                char[] c = new char[(int) file.length()];
+                ArrayList<String> gameState = new ArrayList<>();
+                int size = fw.read(c);
 
-        JPanel panel = new JPanel(new BorderLayout());
-        JTextField textField = new JTextField();
-        JButton button = new JButton("Загрузить");
+                String stat = "";
+                for (int i = 0; i < size; i++) {
+                    if (c[i] == " ".charAt(0)) {
+                        gameState.add(stat);
+                        stat = "";
+                        continue;
+                    }
+                    stat = stat + c[i];
+                }
+                fw.close();
+                gameWindow.setSize(Integer.parseInt(gameState.get(5)), Integer.parseInt(gameState.get(6)));
+                gameWindow.getVisualizer().setPosition(gameState);
+            } catch (Exception e) {
 
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                loadState(saveWindow, textField);
             }
-        });
-
-        panel.add(button, BorderLayout.SOUTH);
-        panel.add(textField, BorderLayout.NORTH);
-        saveWindow.add(panel);
-        return saveWindow;
-
+        }
     }
 
-    public void saveState(JInternalFrame saveWindow, JTextField textField, ArrayList<String> stateBeforeSave){
-        String fileName = "src//saves//"+ textField.getText() + ".txt";
-        File file = new File(fileName);
+    private void setLookAndFeel(String className) {
         try {
-            file.createNewFile();
-            FileWriter writer = new FileWriter(fileName, false);
-            ArrayList<String> state = gameWindow.getVisualizer().getGameState();
-            for(int i = 0; i < state.size(); i++){
-                writer.write(state.get(i));
-                writer.write("\n");
-            }
-            writer.flush();
-            writer.close();
-            saveWindow.dispose();
-            gameWindow.getVisualizer().isEditor = false;
-            gameWindow.getVisualizer().setPosition(stateBeforeSave);
-            gameWindow.getVisualizer().setTimer();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
-
-    public void loadState(JInternalFrame saveWindow, JTextField textField){
-        String fileName = "src//saves//"+ textField.getText() + ".txt";
-        try {
-            FileReader reader = new FileReader(fileName);
-            Scanner scan = new Scanner(reader);
-            ArrayList<String> gameState = new ArrayList<String>();
-            while (scan.hasNextLine()) {
-                gameState.add(scan.nextLine());
-            }
-            reader.close();
-            gameWindow.setSize(Integer.parseInt(gameState.get(5)), Integer.parseInt(gameState.get(6)));
-            gameWindow.getVisualizer().setPosition(gameState);
-            saveWindow.dispose();
-        }catch (FileNotFoundException e2) {
-            JOptionPane.showMessageDialog(saveWindow, "Файла с таким именем нет!",
-                    "Ошибка загрузки", JOptionPane.WARNING_MESSAGE);
-        }
-        catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
-
-    private void setLookAndFeel(String className)
-    {
-        try
-        {
             UIManager.setLookAndFeel(className);
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        catch (ClassNotFoundException | InstantiationException
-                | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
+        } catch (ClassNotFoundException | InstantiationException
+                | IllegalAccessException | UnsupportedLookAndFeelException e) {
             // just ignore
         }
     }
